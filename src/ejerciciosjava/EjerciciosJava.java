8d342bea944dc7117211f0b271fe1986d19956a1
xp <xp@CETYSDAM026>
2018-01-31 17:13:19 +0100

    Faltan espacios

--- src/ejerciciosjava/EjerciciosJava.java
+++ src/ejerciciosjava/EjerciciosJava.java
@@ -31,12 +31,20 @@ public class EjerciciosJava
         return numerosMaximos;
     }
     
-    public boolean palindromo (String pali)
+    private boolean palindromo (String pali)
     {
         String a = "";
+        String cancer = pali.toUpperCase();
+        cancer = cancer.replaceAll("Ñ", "N");
+        cancer = cancer.replaceAll("Á", "A");
+        cancer = cancer.replaceAll("É", "E");
+        cancer = cancer.replaceAll("Í", "I");
+        cancer = cancer.replaceAll("Ó", "O");
+        cancer = cancer.replaceAll("Ú", "U");
+        cancer = cancer.replaceAll("\\W", "");
         for(int i=0; i<pali.length();i++)
         {
-            if(pali.charAt(i) != ' ')
+            if(pali.charAt(i) != '_')
             {
                 a = a + pali.charAt(i);
             }
@@ -48,6 +56,12 @@ public class EjerciciosJava
             b = b + pali.charAt(sida-i-1);
         }
         return pali.equals(b);
+    }
+    
+    private boolean isograma (String iso)
+    {
+        
+        return false;
     }
     
     /**
@@ -72,7 +86,7 @@ public class EjerciciosJava
         int[] lista = {50,31,27,2,5,99};
         int[] lista2 = {150,31,27,2,5,99};
         String palin = "ACASO HUBO BUHOS ACA"; //20 chars
-        String palin2 ="ANNA";
+        String palin2 ="ANA";
 //        ejercicio1.ejemplosString();
 //        System.out.println(Arrays.toString(ejercicio1.maximo(lista)));
 //        System.out.println(Arrays.toString(ejercicio1.maximo(lista2)));
